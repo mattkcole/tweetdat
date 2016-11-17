54b5d79ca000c06bbaf60a99310e1b68305e8b44
matt cole <mattcole1093@gmail.com>
2016-11-16 22:55:34 -0500

    first commit

---
+++ R/hello.R
@@ -0,0 +1,172 @@
+# install.packages(c("devtools", "roxygen2", "testthat", "knitr"))
+# library("devtools")
+# library("roxygen2")
+# library("testthat")
+# library("knitr")
+
+#
+# Some useful keyboard shortcuts for package authoring:
+#
+#   Build and Reload Package:  'Ctrl + Shift + B'
+#   Check Package:             'Ctrl + Shift + E'
+#   Test Package:              'Ctrl + Shift + T'
+
+
+normalize <- function(w){
+        w$replyToSN <- as.character(w$replyToSN)
+        w$replyToSID <- as.character(w$replyToSID)
+        w$replyToUID <- as.character(w$replyToUID)
+        w
+}
+
+reduce_tweet <- function(x){
+        x <- select(x, -id, -replyToUID)
+        x
+}
+
+get_those_tweets_meow <- function(sample_size = 150, term = '@hillaryclinton'){
+
+        tweets <- searchTwitter(term, n=sample_size)
+        tweets_time <- Sys.time()
+
+        # lets generate a list (vector) of all of the observed users
+
+        user_names <- vector(length = sample_size)
+        for (i in 1:sample_size){
+                user_names[i] <- tweets[[i]]$screenName
+        }
+        user_names <- as.vector(user_names)
+
+        # lets get info from these users
+        sample_users <- lookupUsers(user_names)
+        sample_users_df <- twListToDF(sample_users)
+
+        # lets make a data frame of the tweets we got
+        tweets_df <- twListToDF(tweets)
+        #head(tweets_df)
+        #colnames(tweets_df)
+
+        # making a list of all users and one tweet (not particularly usefull)
+        tester <- full_join(sample_users_df, tweets_df, by='screenName')
+        tester <- select(tester, -id.y, -id.x)
+
+        print("Collecting Tweets...")
+
+        list_dim <- data.frame()
+        tweet_count <- 10
+        for (i in 1:length(user_names)){
+                w <- userTimeline(user_names[i],n=tweet_count, includeRts=TRUE)
+                if (identical(w, list()) == FALSE){
+                        tempor <- twListToDF(w) # this is a new line...
+                        tempor <- normalize(tempor)
+                        list_dim <- bind_rows(tempor, list_dim)
+                        Sys.sleep(0)
+                }
+        }
+
+        tweet_observations <- list_dim
+        # lets say that our data is in list_dim
+        # we will now attempt to add tweet info to our data rows
+
+        print("Making those tweets pretty")
+
+        for (i in 1:tweet_count){
+                if (i == 1){
+                        to_add <- match(unique(tweet_observations$screenName), tweet_observations$screenName)
+                        #tweet_observations <- reduce_tweet(tweet_observations)
+
+                        add_me <- tweet_observations[to_add,]
+                        colnames(add_me) <- ifelse(colnames(tweet_observations) == "screenName", # TEST
+                                                   colnames(tweet_observations), paste(colnames(tweet_observations),i,sep=""))# TEST
+
+                        db <- full_join(sample_users_df, add_me, by='screenName') #changing tweet_observations[to_add,] to add_me
+                } else {
+                        # removing those observations already added
+                        tweet_observations <- tweet_observations[-to_add,]
+
+                        # finding new unique observations to add
+                        to_add <- match(unique(tweet_observations$screenName), tweet_observations$screenName)
+                        # storing these
+                        add_me <- tweet_observations[to_add,]
+                        # renaming
+                        colnames(add_me) <- ifelse(colnames(tweet_observations) == "screenName",
+                                                   colnames(tweet_observations), paste(colnames(tweet_observations),i,sep=""))
+                        db <- full_join(db, add_me, by='screenName')
+                }
+        }
+        print(paste("Data was generated from twitter on", tweets_time))
+        return(db)
+}
+
+# Existing trolls to DF
+
+trolls <- function(usernames){
+
+        # lets get info from these users
+        sample_users <- lookupUsers(usernames)
+        sample_users_df <- twListToDF(sample_users)
+
+
+        # defining this function to allow for easier data frame construction
+        normalize <- function(w){
+                w$replyToSN <- as.character(w$replyToSN)
+                w$replyToSID <- as.character(w$replyToSID)
+                w$replyToUID <- as.character(w$replyToUID)
+                w
+        }
+
+        reduce_tweet <- function(x){
+                x <- select(x, -id, -replyToUID)
+                x
+        }
+
+        list_dim <- data.frame()
+        tweet_count <- 10
+        for (i in 1:length(user_names)){
+                w <- userTimeline(user_names[i],n=tweet_count, includeRts=TRUE)
+                if (identical(w, list()) == FALSE){
+                        tempor <- twListToDF(w) # this is a new line...
+                        tempor <- normalize(tempor)
+                        list_dim <- bind_rows(tempor, list_dim)
+                        Sys.sleep(0)
+                }
+        }
+
+        tweet_observations <- list_dim
+        # lets say that our data is in list_dim
+        # we will now attempt to add tweet info to our data rows
+
+        tweetno <- tweet_count
+        print(paste("We will collect the most recent", tweetno, "tweets"))
+
+        for (i in 1:tweetno){
+                if (i == 1){
+                        to_add <- match(unique(tweet_observations$screenName), tweet_observations$screenName)
+                        #tweet_observations <- reduce_tweet(tweet_observations)
+
+                        add_me <- tweet_observations[to_add,]
+                        colnames(add_me) <- ifelse(colnames(tweet_observations) == "screenName", # TEST
+                                                   colnames(tweet_observations), paste(colnames(tweet_observations),i,sep=""))# TEST
+
+                        #                 colnames(add_me) <- ifelse(colnames(add_me) == "screenName",# PRESERVED
+                        #                         colnames(add_me), paste(colnames(add_me),i,sep="")) # PRESERVED
+                        db <- full_join(sample_users_df, add_me, by='screenName') #changing tweet_observations[to_add,] to add_me
+                } else {
+                        # removing those observations already added
+                        tweet_observations <- tweet_observations[-to_add,]
+
+                        # finding new unique observations to add
+                        to_add <- match(unique(tweet_observations$screenName), tweet_observations$screenName)
+                        # storing these
+                        add_me <- tweet_observations[to_add,]
+                        # renaming
+                        colnames(add_me) <- ifelse(colnames(tweet_observations) == "screenName",
+                                                   colnames(tweet_observations), paste(colnames(tweet_observations),i,sep=""))
+                        db <- full_join(db, add_me, by='screenName')
+                }
+        }
+
+        paste("Data was generated from twitter on", tweets_time)
+        return(db)
+
+}
